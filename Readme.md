9f93c03fc5e694d01e7d8160d533be0a131f6342
Dave Trencher <dave@thetrenchers.co.uk>
2018-11-28 21:58:16 +0000

    Initial Project setup
    - Added gradle build files
    - Added Application class
    - Added Readme stub

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Serverside Test
+
+## About
+
+https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html
+
+
+## Requirements
+
+ * Java 8
+ 
+## Setup
+
+Build the project
+
+```
+
+./gradlew clean builld
+
+```
+Run the project
+
+```
+ java -jar build/libs/serverside-test-0.0.1.jar
+```
---
+++ src/main/java/com/github/davetrencher/Application.java
@@ -0,0 +1,21 @@
+package com.github.davetrencher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is the initial entry point to the application.
+ *
+ * The Application calls a URL and then processes the data returning data as JSON.
+ *
+ */
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+
+
+    }
+}
